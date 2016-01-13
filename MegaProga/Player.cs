629c0de8d5539aa3659fcb1bb607da1218c89a18
y <dd>
2016-01-13 11:12:16 +0300

    jfdjfh

---
+++ MegaProga/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaProga
+{
+    class Player
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int TeamID { get; set; }
+    }
+}
