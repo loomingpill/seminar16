fe3132f2c9d996d882bc718d6b7f13a7014aef28
Vinyl <bla@mail.ru>
2016-01-13 11:18:36 +0300

    Task_5

---
+++ MegaProga/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaProga
+{
+    class Context : DbContext
+    {
+        DbSet <Player> Players;
+        DbSet <Team> Teams;
+    }
+}
