ea1a6bf759d783cf7a63f2c8ba45d13d2f242190
Vinyl <bla@mail.ru>
2016-01-13 11:27:13 +0300

    Different_Cng

--- MegaProga/Player.cs
+++ MegaProga/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace MegaProga
     class Player
     {
         public int ID { get; set; }
+        [MaxLength(69)]
         public string Name { get; set; }
         public int TeamID { get; set; }
     }
